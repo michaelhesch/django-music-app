22e8f64afeb9742ef1ddad3a3fe3e84e9ffb46fc
michaelhesch <michael.hesch@gmail.com>
2021-11-05 20:12:20 +0000

    Finalize initial python api setup

--- music_app/api/views.py
+++ music_app/api/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+
+from api.serializers import RoomSerializer
+
+from .models import Room
+from .serializers import RoomSerializer
 
 
-def main(request):
-    return HttpResponse("Hello")
+# API view to return list of all available rooms
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
